c253888c2901ffe382f3537c0cf80b3434d36205
Ilham Putra Husada <hambosto@gmail.com>
2025-03-15 02:00:36 +0700

    refactor(hyprland): update hyprland colors config

--- nix/module.nix
+++ nix/module.nix
@@ -73,24 +73,10 @@ in
     '';
 
     home.file.".config/wallust/templates/hyprland-colors.conf".text = ''
-      $background = rgb({{background | strip}})
-      $foreground = rgb({{foreground | strip}})
-      $color0 = rgb({{color0 | strip}})
-      $color1 = rgb({{color1 | strip}})
-      $color2 = rgb({{color2 | strip}})
-      $color3 = rgb({{color3 | strip}})
-      $color4 = rgb({{color4 | strip}})
-      $color5 = rgb({{color5 | strip}})
-      $color6 = rgb({{color6 | strip}})
-      $color7 = rgb({{color7 | strip}})
-      $color8 = rgb({{color8 | strip}})
-      $color9 = rgb({{color9 | strip}})
-      $color10 = rgb({{color10 | strip}})
-      $color11 = rgb({{color11 | strip}})
-      $color12 = rgb({{color12 | strip}})
-      $color13 = rgb({{color13 | strip}})
-      $color14 = rgb({{color14 | strip}})
-      $color15 = rgb({{color15 | strip}})
+      general {
+          col.active_border = rgb({{color1 | saturate(0.6) | strip}}) rgb({{color2 | saturate(0.6) | strip}}) rgb({{color3 | saturate(0.6) | strip}}) rgb({{color4 | saturate(0.6) | strip}}) rgb({{color5 | saturate(0.6) | strip}}) rgb({{color6 | saturate(0.6) | strip}})
+          col.inactive_border = rgba({{color0}})
+      }
     '';
 
     # Hyprland integration
@@ -98,12 +84,6 @@ in
       extraConfig = ''
         source = ${config.xdg.configHome}/hypr/hyprland-colors.conf
       '';
-      settings = {
-        general = {
-          "col.active_border" = "$color12";
-          "col.inactive_border" = "$color10";
-        };
-      };
     };
 
     home.packages = [ self.packages.${pkgs.system}.default ];
