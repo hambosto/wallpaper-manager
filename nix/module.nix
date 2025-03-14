c9e9026221d3b7ad47c9afcc5ee19a5b99a2b288
Ilham Putra Husada <hambosto@gmail.com>
2025-03-15 01:44:28 +0700

    chore(nix): remove wallust configuration

--- nix/module.nix
+++ nix/module.nix
@@ -65,33 +65,33 @@ in
       };
     };
 
-    # Wallust configuration
-    home.file.".config/wallust/wallust.toml".text = ''
-      [templates]
-      hypr.template = 'hyprland-colors.conf'
-      hypr.target = '~/.config/hypr/hyprland-colors.conf'
-    '';
+    # # Wallust configuration
+    # home.file.".config/wallust/wallust.toml".text = ''
+    #   [templates]
+    #   hypr.template = 'hyprland-colors.conf'
+    #   hypr.target = '~/.config/hypr/hyprland-colors.conf'
+    # '';
 
-    home.file.".config/wallust/templates/hyprland-colors.conf".text = ''
-      $background = rgb({{background | strip}})
-      $foreground = rgb({{foreground | strip}})
-      $color0 = rgb({{color0 | strip}})
-      $color1 = rgb({{color1 | strip}})
-      $color2 = rgb({{color2 | strip}})
-      $color3 = rgb({{color3 | strip}})
-      $color4 = rgb({{color4 | strip}})
-      $color5 = rgb({{color5 | strip}})
-      $color6 = rgb({{color6 | strip}})
-      $color7 = rgb({{color7 | strip}})
-      $color8 = rgb({{color8 | strip}})
-      $color9 = rgb({{color9 | strip}})
-      $color10 = rgb({{color10 | strip}})
-      $color11 = rgb({{color11 | strip}})
-      $color12 = rgb({{color12 | strip}})
-      $color13 = rgb({{color13 | strip}})
-      $color14 = rgb({{color14 | strip}})
-      $color15 = rgb({{color15 | strip}})
-    '';
+    # home.file.".config/wallust/templates/hyprland-colors.conf".text = ''
+    #   $background = rgb({{background | strip}})
+    #   $foreground = rgb({{foreground | strip}})
+    #   $color0 = rgb({{color0 | strip}})
+    #   $color1 = rgb({{color1 | strip}})
+    #   $color2 = rgb({{color2 | strip}})
+    #   $color3 = rgb({{color3 | strip}})
+    #   $color4 = rgb({{color4 | strip}})
+    #   $color5 = rgb({{color5 | strip}})
+    #   $color6 = rgb({{color6 | strip}})
+    #   $color7 = rgb({{color7 | strip}})
+    #   $color8 = rgb({{color8 | strip}})
+    #   $color9 = rgb({{color9 | strip}})
+    #   $color10 = rgb({{color10 | strip}})
+    #   $color11 = rgb({{color11 | strip}})
+    #   $color12 = rgb({{color12 | strip}})
+    #   $color13 = rgb({{color13 | strip}})
+    #   $color14 = rgb({{color14 | strip}})
+    #   $color15 = rgb({{color15 | strip}})
+    # '';
 
     # # Hyprland integration
     # wayland.windowManager.hyprland = lib.mkIf config.programs.wallpaper-manager.hyprland.enable {
