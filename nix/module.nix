15f178e6284a9e6b9904515cbb83aef0ff11abed
Ilham Putra Husada <hambosto@gmail.com>
2025-03-18 02:54:29 +0700

    feat(integrations): add rofi integration

--- nix/module.nix
+++ nix/module.nix
@@ -79,6 +79,7 @@ in
       integrations = {
         hyprland = mkEnableOption "Wallust integration with Hyprland";
         kitty = mkEnableOption "Wallust integration with Kitty terminal";
+        rofi = mkEnableOption "Wallust integration with Rofi";
       };
     };
   };
@@ -147,6 +148,10 @@ in
               kitty.template = "kitty-colors.conf"
               kitty.target = "${config.xdg.configHome}/kitty/themes/kitty-colors.conf"
             ''}
+            ${optionalString cfg.wallust.integrations.rofi ''
+              rofi.template = "rofi-colors.rasi"
+              rofi.target = "${config.xdg.configHome}/rofi/themes/rofi-colors.rasi"
+            ''}
           '';
         };
 
@@ -206,6 +211,31 @@ in
             color14     {{color14}}
             color15     {{color15}}
           '';
+
+          "wallust/templates/rofi-colors.rasi" = mkIf cfg.wallust.integrations.rofi {
+            text = ''
+              * {{
+                  background: rgba(0,0,1,0.5);
+                  foreground: #FFFFFF;
+                  color0:     {color0};
+                  color1:     {color1};
+                  color2:     {color2};
+                  color3:     {color3};
+                  color4:     {color4};
+                  color5:     {color5};
+                  color6:     {color6};
+                  color7:     {color7};
+                  color8:     {color8};
+                  color9:     {color9};
+                  color10:    {color10};
+                  color11:    {color11};
+                  color12:    {color12};
+                  color13:    {color13};
+                  color14:    {color14};
+                  color15:    {color15};
+                }}
+            '';
+          };
         };
       };
     })
