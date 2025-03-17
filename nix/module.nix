7c55d46a807433af9ea1e9fa930bedf0b8c89682
Ilham Putra Husada <hambosto@gmail.com>
2025-03-18 04:31:33 +0700

    refactor(rofi): simplify rofi color scheme variables

--- nix/module.nix
+++ nix/module.nix
@@ -215,24 +215,17 @@ in
         };
         "wallust/templates/rofi-colors.rasi" = mkIf cfg.wallust.integrations.rofi {
           text = ''
-            * {{
-                color0: {{color0}};
-                color1: {{color1}};
-                color2: {{color2}};
-                color3: {{color3}};
-                color4: {{color4}};
-                color5: {{color5}};
-                color6: {{color6}};
-                color7: {{color7}};
-                color8: {{color8}};
-                color9: {{color9}};
-                color10: {{color10}};
-                color11: {{color11}};
-                color12: {{color12}};
-                color13: {{color13}};
-                color14: {{color14}};
-                color15: {{color15}};
-              }}
+            * {
+              accent: {{color5}};
+              bg-col: {{color6}};
+              bg-col-light: {{color7}};
+              blue: {{color8}};
+              border-col: {{color9}};
+              fg-col: {{color10}};
+              fg-col2: {{color11}};
+              grey: {{color12}};
+              selected-col: {{color13}};
+            }
           '';
         };
       };
